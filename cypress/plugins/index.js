d78e584be7c01f62811b126fa68e5776d18dbb5f
Tony CABAYE <tony.cabaye@smile.fr>
2020-10-12 10:43:59 +0200

    tests: fix borwser viewport for screenshots

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -2,5 +2,28 @@ const { initPlugin } = require('cypress-plugin-snapshots/plugin');
 
 module.exports = (on, config) => {
   initPlugin(on, config);
+
+  const { viewportWidth: w, viewportHeight: h } = config;
+  on('before:browser:launch', (browser = {}, launchOptions) => {
+    switch (browser.name) {
+      //browser.family === 'chromium' && browser.name !== 'electron')
+      case 'chrome':
+        launchOptions.args.push(`--window-size=${w},${h}`);
+        /*
+        launchOptions.push('--cast-initial-screen-width=1600')
+        launchOptions.push('--cast-initial-screen-height=900')
+         */
+        break;
+
+      case 'electron':
+        launchOptions.preferences.width = w;
+        launchOptions.preferences.height = h;
+        break;
+
+      default:
+    }
+    return launchOptions;
+  });
+
   return config;
 };
